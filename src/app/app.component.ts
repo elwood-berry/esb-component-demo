a1de73b80b18ab2ae434736f66fc64734f279e3d
elwood-berry <eberry@anterasoftware.com>
2021-07-10 17:54:50 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,44 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  // SAMPLE LISTING
+  public sampleListings = [
+    {
+      listingId: 0,
+      broker: 'broker name goes here',
+      new: false,
+      openHouseDate: 'MM/DD',
+      status: ['For Sale'],
+      imgHome: [
+        {
+          url: 'www.domain.com'
+        }
+      ],
+      imgBroker: [
+        {
+          url: 'www.domain.com'
+        }
+      ],
+      price: 1000000,
+      bed: 100,
+      bath: 100,
+      sqft: 10000,
+      sqftlot: 10000,
+      likes: 1,
+      url: 'www.domain.com',
+      address1: '1000 Somewhere Dr.',
+      address2: 'Suite 420',
+      city: 'Dallas',
+      state: [
+        {
+          abbrv: 'TX',
+          full: 'Texas'
+        }
+      ]
+    }
+  ];
+}
 
 /*
 -------------------------------------------------

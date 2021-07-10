67663c803cf7a57a0e39685e7c518a11f6fad5a7
elwood-berry <eberry@anterasoftware.com>
2021-07-10 18:47:23 -0500

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,37 +15,49 @@ export class AppComponent {
   // SAMPLE LISTING
   public sampleListings = [
     {
-      listingId: 0,
-      broker: 'broker name goes here',
-      new: false,
-      openHouseDate: 'MM/DD',
-      status: ['For Sale'],
-      imgHome: [
-        {
-          url: 'www.domain.com'
-        }
-      ],
+      address1: '1000 Somewhere Dr.',
+      address2: 'Suite 420',
+      bath: 100,
+      bed: 100,
+      broker: '99 broker name goes here',
+      city: 'Dallas',
       imgBroker: [
         {
           url: 'www.domain.com'
         }
       ],
+      imgHome: [
+        {
+          imgHomeId: 0,
+          imgName: 'Name of the image goes here.',
+          imgDescription: 'Description of the image goes here.',
+          url:
+            'https://ap.rdcpix.com/1269221619/88ab3e5ba67115f2bc21dbc9082db210o-c0s.jpg'
+        },
+        {
+          imgHomeId: 1,
+          imgName: 'Name of the image goes here.',
+          imgDescription: 'Description of the image goes here.',
+          url:
+            'https://ap.rdcpix.com/1269221619/88ab3e5ba67115f2bc21dbc9082db210o-c0s.jpg'
+        }
+      ],
+      likes: 1,
+      listingId: 0,
+      new: true,
+      openHouseDate: 'MM/DD',
       price: 1000000,
-      bed: 100,
-      bath: 100,
       sqft: 10000,
       sqftlot: 10000,
-      likes: 1,
-      url: 'www.domain.com',
-      address1: '1000 Somewhere Dr.',
-      address2: 'Suite 420',
-      city: 'Dallas',
       state: [
         {
           abbrv: 'TX',
           full: 'Texas'
         }
-      ]
+      ],
+      status: ['For Sale'],
+      url: 'www.domain.com',
+      zip: 70000
     }
   ];
 }
--- src/app/components/home-card/home-card.component.ts
+++ src/app/components/home-card/home-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home-card',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-card.component.scss']
 })
 export class HomeCardComponent implements OnInit {
+  // INPUT Properties
+  @Input() address1 = '';
+  @Input() address2 = '';
+  @Input() bath = null;
+  @Input() bed = null;
+  @Input() broker = '';
+  @Input() city = '';
+  @Input() imgBroker = '';
+  @Input() imgHome = '';
+  @Input() likes = null;
+  @Input() listingId = null;
+  @Input() new = false;
+  @Input() openHouseDate = '';
+  @Input() price = null;
+  @Input() sqft = null;
+  @Input() sqftlot = null;
+  @Input() state = '';
+  @Input() status = [];
+  @Input() url = '';
+  @Input() zip = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

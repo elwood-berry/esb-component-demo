d61f5991fdd812df20ee6753e376834969db1925
elwood-berry <eberry@anterasoftware.com>
2021-07-10 15:23:54 -0500

    3 files modified, 1 added, 1 deleted

--- src/app/components/home-card/home-card.component.ts
+++ src/app/components/home-card/home-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-card',
   templateUrl: './home-card.component.html',
-  styleUrls: ['./home-card.component.css']
+  styleUrls: ['./home-card.component.scss']
 })
 export class HomeCardComponent implements OnInit {
 

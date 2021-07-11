d949e8613a7ff3b1347b40d4af61366291db4657
elwood-berry <eberry@anterasoftware.com>
2021-07-10 20:51:17 -0500

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // SAMPLE LISTING
   public sampleListings = [
+    // LISTING #1 -------------------------------------------
     {
       address1: '1000 Somewhere Dr.',
       address2: 'Suite 420',
@@ -62,6 +63,108 @@ export class AppComponent {
       status: ['For Sale'],
       url: 'www.domain.com',
       zip: 70000
+    },
+
+    // LISTING #2 -------------------------------------------
+    {
+      address1: '7985 Thistletree Ln',
+      address2: '',
+      bath: 2.5,
+      bed: 4,
+      broker: 'Fat Cat Realty',
+      city: 'Frisco',
+      imgBroker: [
+        {
+          imgBrokerId: 0,
+          imgName: 'Name of the image goes here.',
+          imgDescription: 'Description of the image goes here.',
+          url:
+            'https://ap.rdcpix.com/1269221619/88ab3e5ba67115f2bc21dbc9082db210o-c0s.jpg'
+        }
+      ],
+      imgHome: [
+        {
+          imgHomeId: 0,
+          imgName: 'Name of the image goes here.',
+          imgDescription: 'Description of the image goes here.',
+          url:
+            'https://ap.rdcpix.com/d15d9e27e1fac7c2f85810f41e8cae06l-m3511790724od-w480_h360.jpg'
+        },
+        {
+          imgHomeId: 1,
+          imgName: 'Name of the image goes here.',
+          imgDescription: 'Description of the image goes here.',
+          url:
+            'https://ap.rdcpix.com/44ba5e8fcd0439cbe78b772b6ed1d482l-m1257779790od-w480_h360.jpg'
+        }
+      ],
+      likes: 1,
+      listingId: 1,
+      new: true,
+      openHouseDate: 'MM/DD',
+      price: 495000,
+      sqft: 3069,
+      sqftlot: 0.29,
+      state: [
+        {
+          abbrv: 'TX',
+          full: 'Texas'
+        }
+      ],
+      status: ['For Sale'],
+      url: 'www.domain.com',
+      zip: 75033
+    },
+
+    // LISTING #3 -------------------------------------------
+    {
+      address1: '4136 Los Altos Dr',
+      address2: '',
+      bath: 3,
+      bed: 4,
+      broker: 'Supreme Broker',
+      city: 'Plano',
+      imgBroker: [
+        {
+          imgBrokerId: 0,
+          imgName: 'Name of the image goes here.',
+          imgDescription: 'Description of the image goes here.',
+          url:
+            'https://ap.rdcpix.com/1269221619/88ab3e5ba67115f2bc21dbc9082db210o-c0s.jpg'
+        }
+      ],
+      imgHome: [
+        {
+          imgHomeId: 0,
+          imgName: 'Name of the image goes here.',
+          imgDescription: 'Description of the image goes here.',
+          url:
+            'https://ap.rdcpix.com/a252277401d0237fd64b64f3ed330705l-m3438727031od-w480_h360.jpg'
+        },
+        {
+          imgHomeId: 1,
+          imgName: 'Name of the image goes here.',
+          imgDescription: 'Description of the image goes here.',
+          url:
+            'https://ap.rdcpix.com/44ba5e8fcd0439cbe78b772b6ed1d482l-m1257779790od-w480_h360.jpg'
+        }
+      ],
+      likes: 1,
+      listingId: 0,
+      new: true,
+      openHouseDate: 'MM/DD',
+      price: 499000,
+      sqft: 3095,
+      sqftlot: 0.26,
+      state: [
+        {
+          abbrv: 'TX',
+          full: 'Texas'
+        }
+      ],
+      status: ['For Sale'],
+      url: 'www.domain.com',
+      zip: 75024
     }
   ];
 }
